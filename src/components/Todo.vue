c0fb9c71e0a558b3ea6b2acfb1ca4e18eb2875dc
Callum Laing <callum.laing91@hotmail.co.uk>
2025-05-27 19:58:36 +0100

    updated todo list to include edit function

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -7,6 +7,9 @@ const newTodo = ref('');
 const hideCompleted = ref(false);
 const todos = ref([]);
 
+const editingId = ref(null);
+const editingText = ref('');
+
 onMounted(() => {
 	const savedTodos = localStorage.getItem('todos');
 	const savedHideCompleted = localStorage.getItem('hideCompleted');
@@ -47,6 +50,22 @@ function addTodo() {
 function removeTodo(todo) {
 	todos.value = todos.value.filter((t) => t !== todo);
 }
+
+function startEditing(todo) {
+	editingId.value = todo.id;
+	editingText.value = todo.text;
+}
+
+function saveEdit(todo) {
+	if (editingText.value.trim()) {
+		todo.text = editingText.value.trim();
+		editingId.value = null;
+	}
+}
+
+function cancelEdit() {
+	editingId.value = null;
+}
 </script>
 
 <template>
@@ -61,8 +80,18 @@ function removeTodo(todo) {
 		<ul class="todoList">
 			<li v-for="todo in filteredTodos" :key="todo.id">
 				<input type="checkbox" v-model="todo.done" />
-				<span :class="{ done: todo.done }">{{ todo.text }}</span>
-				<button class="btn btnRemove" @click="removeTodo(todo)">X</button>
+
+				<template v-if="editingId === todo.id">
+					<input v-model="editingText" class="editInput" />
+					<button class="btn btnSave" @click="saveEdit(todo)">Save</button>
+					<button class="btn btnCancel" @click="cancelEdit">Cancel</button>
+				</template>
+
+				<template v-else>
+					<span :class="{ done: todo.done }">{{ todo.text }}</span>
+					<button class="btn btnEdit" @click="startEditing(todo)">Edit</button>
+					<button class="btn btnRemove" @click="removeTodo(todo)">X</button>
+				</template>
 			</li>
 		</ul>
 
@@ -95,7 +124,7 @@ h2 {
 	gap: 10px;
 	margin-bottom: 1rem;
 	width: 100%;
-	max-width: 400px;
+	max-width: 600px;
 
 	input {
 		padding: 8px;
@@ -118,7 +147,7 @@ h2 {
 	list-style: none;
 	padding: 0;
 	width: 100%;
-	max-width: 400px;
+	max-width: 600px;
 
 	li {
 		display: flex;
@@ -130,6 +159,7 @@ h2 {
 		border-radius: 5px;
 		border: 1px solid rgba(150, 150, 150, 0.5);
 		box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+		flex-wrap: wrap;
 
 		input[type='checkbox'] {
 			margin-right: 10px;
@@ -142,9 +172,18 @@ h2 {
 		}
 
 		button {
-			margin-left: 10px;
+			margin-left: 5px;
+			margin-top: 5px;
 		}
 	}
+}
+
+.editInput {
+	flex: 1;
+	padding: 6px;
+	font-size: 1em;
+	border: 1px solid #ccc;
+	border-radius: 4px;
 }
 
 .done {
@@ -185,6 +224,21 @@ h2 {
 .btnHide {
 	--btn-color: #457b9d;
 	color: #457b9d;
+}
+
+.btnEdit {
+	--btn-color: #f4a261;
+	color: #f4a261;
+}
+
+.btnSave {
+	--btn-color: #2a9d8f;
+	color: #2a9d8f;
+}
+
+.btnCancel {
+	--btn-color: #999;
+	color: #999;
 }
 
 @media (max-width: 480px) {
